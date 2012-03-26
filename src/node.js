19a897a0a8e1af8f62bbdedf5bba8d18ea52ed07
Nathan Rajlich <nathan@tootallnate.net>
2012-03-26 15:00:35 -0700

    src: use the 'terminal' option

--- src/node.js
+++ src/node.js
@@ -123,7 +123,7 @@
         // REPL
         var repl = Module.requireRepl().start({
           prompt: '> ',
-          enabled: !parseInt(process.env['NODE_NO_READLINE'], 10),
+          terminal: !parseInt(process.env['NODE_NO_READLINE'], 10),
           useGlobal: true,
           ignoreUndefined: false
         });
