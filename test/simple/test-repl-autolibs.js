d3811ab179bd396dcc2fb01768431007becfa167
Nathan Rajlich <nathan@tootallnate.net>
2012-06-30 20:31:00 -0700

    repl: attempt to require() the command when a ReferenceError occurs
    This is a re-implementation of b72277183f9cb79274d6f8915e02b895c56b2288
    to be more robust:
    
     - fixes #3564
     - it's now a regular require() call under the hood, so it works with modules
       in ./node_modules and $NODE_PATH, etc.
     - logic is cleaner and in a more appropriate place in the repl

--- test/simple/test-repl-autolibs.js
+++ test/simple/test-repl-autolibs.js
@@ -52,28 +52,9 @@ function test1(){
       assert.equal(data, util.inspect(require('fs'), null, null, false) + '\n');
       // globally added lib matches required lib
       assert.equal(global.fs, require('fs'));
-      test2();
     }
   };
   assert(!gotWrite);
   putIn.run(['fs']);
-  assert(gotWrite);
-}
-
-function test2(){
-  var gotWrite = false;
-  putIn.write = function(data) {
-    gotWrite = true;
-    if (data.length) {
-      // repl response error message
-      assert.equal(data.indexOf('A different'), 0);
-      // original value wasn't overwritten
-      assert.equal(val, global.url);
-    }
-  };
-  var val = {};
-  global.url = val;
-  assert(!gotWrite);
-  putIn.run(['url']);
   assert(gotWrite);
 }
